139adc8dd79c6e0c4172b0c7bb2eb569fed7c94b
TheAlyxGreen <3075793+TheAlyxGreen@users.noreply.github.com>
2023-04-19 14:42:42 -0500

    Made script retry after failures without needing to manually restart

--- defendTrans.py
+++ defendTrans.py
@@ -6,11 +6,11 @@ from faker import Faker
 
 import address
 
-
 fake = Faker()
 URL = "https://ago.mo.gov/file-a-complaint/transgender-center-concerns?sf_cntrl_id=ctl00$MainContent$C001"
-
+count = 0
 while True:
+    count += 1
     missouri = address.Address.generate_MO_address()
 
     data = {"TextFieldController_4": fake.first_name(),
@@ -28,15 +28,17 @@ while True:
                "User-Agent": fake.user_agent(),
                "X-Forwarded-For": fake.ipv4(),
                "Cookie": ""}
-
-    response = requests.post(URL, data=data_json, headers=headers)
-    if not response.ok:
-        print(f"Endpoint failed {response.status_code}")
-        sys.exit(1)
-    elif "already submitted" in response.text:
-        print("Form already submitted, workaround required")
-        sys.exit(1)
-
-    print(f"Response submitted for {data['TextFieldController_5']}, {data['TextFieldController_4']}")
-
-    time.sleep(1)
+    try:
+        response = requests.post(URL, data=data_json, headers=headers)
+        if not response.ok:
+            print(f"Endpoint failed {response.status_code} (Trying again in 10 seconds)...")
+            sys.exit(1)
+        elif "already submitted" in response.text:
+            print("Form already submitted, workaround required? (Trying again in 10 seconds)...")
+            time.sleep(10)
+        else:
+            print(f"Response {count} submitted for {data['TextFieldController_5']}, {data['TextFieldController_4']}")
+            time.sleep(1)
+    except:
+        print("An error occurred. Trying again in 10 seconds...")
+        time.sleep(10)
